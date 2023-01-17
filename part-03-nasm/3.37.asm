79be5a962cc4e47b1886d41b2c0b97070f029946
Tamerlan Bimzhanov <bimzhanovt.net@gmail.com>
2023-01-18 00:57:19 +0600

    Удалить двойные табуляции в решениях на языке NASM

--- part-03-nasm/3.37.asm
+++ part-03-nasm/3.37.asm
@@ -20,51 +20,53 @@ section .text
 inv_argc_str	db `Invalid argument count!\n`
 inv_argc_strlen	equ $-inv_argc_str
 
-inv_argc:	kernel sys_write, stdout, inv_argc_str, inv_argc_strlen
-		kernel sys_exit, 1
+inv_argc:
+	kernel sys_write, stdout, inv_argc_str, inv_argc_strlen
+	kernel sys_exit, 1
 
 inv_arg_str	db `Invalid argument!\n`
 inv_arg_strlen	equ $-inv_arg_str
 
-inv_arg:	kernel sys_write, stdout, inv_arg_str, inv_arg_strlen
-		kernel sys_exit, 1
+inv_arg:
+	kernel sys_write, stdout, inv_arg_str, inv_arg_strlen
+	kernel sys_exit, 1
 
-_start:		mov ebp, esp
+_start:	mov ebp, esp
 
-		cmp dword [ebp], 3
-		jne inv_argc
+	cmp dword [ebp], 3
+	jne inv_argc
 
-		pcall strlen, dword [ebp+8]
-		pcall strtoin, 8, dword [ebp+8], eax
-		test ecx, ecx
-		jnz inv_arg
-		mov dword [n1], eax
+	pcall strlen, dword [ebp+8]
+	pcall strtoin, 8, dword [ebp+8], eax
+	test ecx, ecx
+	jnz inv_arg
+	mov dword [n1], eax
 
-		pcall strlen, dword [ebp+12]
-		pcall strtoin, 8, dword [ebp+12], eax
-		test ecx, ecx
-		jnz inv_arg
-		mov dword [n2], eax
+	pcall strlen, dword [ebp+12]
+	pcall strtoin, 8, dword [ebp+12], eax
+	test ecx, ecx
+	jnz inv_arg
+	mov dword [n2], eax
 
-		mov word [result], "+ "
-		mov ebx, result+2
-		mov eax, [n1]
-		add eax, [n2]
-		pcall itostrn, 8, eax, ebx
-		add ebx, eax
-		mov byte [ebx], `\n`
-		add ebx, 1
+	mov word [result], "+ "
+	mov ebx, result+2
+	mov eax, [n1]
+	add eax, [n2]
+	pcall itostrn, 8, eax, ebx
+	add ebx, eax
+	mov byte [ebx], `\n`
+	add ebx, 1
 
-		mov word [ebx], "- "
-		add ebx, 2
-		mov eax, [n1]
-		sub eax, [n2]
-		pcall itostrn, 8, eax, ebx
-		add ebx, eax
-		mov byte [ebx], `\n`
-		add ebx, 1
+	mov word [ebx], "- "
+	add ebx, 2
+	mov eax, [n1]
+	sub eax, [n2]
+	pcall itostrn, 8, eax, ebx
+	add ebx, eax
+	mov byte [ebx], `\n`
+	add ebx, 1
 
-		sub ebx, result
-		kernel sys_write, stdout, result, ebx
+	sub ebx, result
+	kernel sys_write, stdout, result, ebx
 
-		kernel sys_exit, 0
+	kernel sys_exit, 0
--- part-03-nasm/strtoin.asm
+++ part-03-nasm/strtoin.asm
@@ -15,84 +15,91 @@ section .text
 ;   ECX = 1  -- invalid numeral system <= 1, > 36
 ;   ECX = 2  -- invalid string
 ;     EAX -- error index
-strtoin:	push ebp
-		mov ebp, esp
-		push ebx
-		push esi
+strtoin:
+	push ebp
+	mov ebp, esp
+	push ebx
+	push esi
 
-		cmp dword [ebp+8], 2
-		jb .inv_numeral_system
-		cmp dword [ebp+8], 36
-		ja .inv_numeral_system
+	cmp dword [ebp+8], 2
+	jb .inv_numeral_system
+	cmp dword [ebp+8], 36
+	ja .inv_numeral_system
 
-		xor ecx, ecx			; ECX = 0 -- index
+	xor ecx, ecx			; ECX = 0 -- index
 
-		cmp dword [ebp+16], 0		; length = 0?
-		jne .nzl			;   NO!
-		jmp short .error		;     YES
+	cmp dword [ebp+16], 0		; length = 0?
+	jne .nzl			;   NO!
+	jmp short .error		;     YES
 
-.nzl:		xor eax, eax			; EAX = 0 -- result
+.nzl:	xor eax, eax			; EAX = 0 -- result
 
-		mov esi, [ebp+12]		; ESI = string
-		xor ebx, ebx			; EBX = 0
+	mov esi, [ebp+12]		; ESI = string
+	xor ebx, ebx			; EBX = 0
 
-		mov dword [negative], 0
+	mov dword [negative], 0
 
-		cmp byte [esi], '+'
-		je .skip_sign
+	cmp byte [esi], '+'
+	je .skip_sign
 
-		cmp byte [esi], '-'
-		jne .again
-		mov dword [negative], 1
+	cmp byte [esi], '-'
+	jne .again
+	mov dword [negative], 1
 
-.skip_sign:	inc ecx
-		cmp ecx, [ebp+16]
-		jae .error
+.skip_sign:
+	inc ecx
+	cmp ecx, [ebp+16]
+	jae .error
 
-.again:		mov bl, [esi+ecx]		; BL = current character
+.again:	mov bl, [esi+ecx]		; BL = current character
 
-		cmp bl, '0'
-		jnae .error
-		cmp bl, '9'
-		jnbe .is_letter
-		sub bl, '0'
-		jmp short .char_check
+	cmp bl, '0'
+	jnae .error
+	cmp bl, '9'
+	jnbe .is_letter
+	sub bl, '0'
+	jmp short .char_check
 
-.is_letter:	cmp bl, 'A'
-		jnae .error
-		cmp bl, 'Z'
-		jnbe .error
-		sub bl, 'A' - 10
+.is_letter:
+	cmp bl, 'A'
+	jnae .error
+	cmp bl, 'Z'
+	jnbe .error
+	sub bl, 'A' - 10
 
-.char_check:	mov dl, [ebp+8]
-		dec dl
-		cmp bl, dl
-		jnbe .error
+.char_check:
+	mov dl, [ebp+8]
+	dec dl
+	cmp bl, dl
+	jnbe .error
 
-.continue:	mul dword [ebp+8]		; EAX = EAX * base
-		add eax, ebx			;   + EBX
+.continue:
+	mul dword [ebp+8]		; EAX = EAX * base
+	add eax, ebx			;   + EBX
 
-		inc ecx				; ECX++
-		cmp ecx, [ebp+16]
-		jb .again
+	inc ecx				; ECX++
+	cmp ecx, [ebp+16]
+	jb .again
 
-		cmp dword [negative], 0
-		je .success
+	cmp dword [negative], 0
+	je .success
 
-.negative:	neg eax
+.negative:
+	neg eax
 
-.success:	xor ecx, ecx			; ECX = 0 -- success
-		jmp short .quit
+.success:
+	xor ecx, ecx			; ECX = 0 -- success
+	jmp short .quit
 
 .inv_numeral_system:
-		mov ecx, 1
-		jmp short .quit
+	mov ecx, 1
+	jmp short .quit
 
-.error:		mov eax, ecx
-		mov ecx, 2
+.error:	mov eax, ecx
+	mov ecx, 2
 
-.quit:		pop esi
-		pop ebx
-		mov esp, ebp
-		pop ebp
-		ret
+.quit:	pop esi
+	pop ebx
+	mov esp, ebp
+	pop ebp
+	ret
