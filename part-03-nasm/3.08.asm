4fc5150370521e80dc497830285d56bdee9f535d
Tamerlan Bimzhanov <bimzhanovt.net@gmail.com>
2023-02-18 14:10:33 +0600

    Форматировать решение задачи 3.08

--- part-03-nasm/3.08.asm
+++ part-03-nasm/3.08.asm
@@ -8,15 +8,22 @@ section .text
 _start:	GETCHAR
 	cmp eax, -1
 	je eof
+
+	; EAX is filled with one byte in AL
 	cmp al, '0'
-	jnae ndigit
+	jnae invalid_input
 	cmp al, '9'
-	jnbe ndigit
+	jnbe invalid_input
 	sub al, '0'
+
+	; Prepare for the loop and avoid iterating for value 0
 	mov ecx, eax
 	jecxz quit
+
 print:	PUTCHAR '*'
 	loop print
+
 quit:	FINISH
 eof:	FINISH 1
-ndigit:	FINISH 2
+invalid_input:
+	FINISH 2
