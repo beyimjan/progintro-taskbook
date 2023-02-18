3ac7c905e1a6ef332976d833846ca3cea5e646a9
Tamerlan Bimzhanov <bimzhanovt.net@gmail.com>
2023-02-18 14:34:41 +0600

    Прокомментировать решение задачи 3.11

--- part-03-nasm/3.11.asm
+++ part-03-nasm/3.11.asm
@@ -5,7 +5,8 @@
 global _start
 
 section .text
-_start:	xor ecx, ecx		; sum of digits
+_start:	xor ecx, ecx		; ECX = 0 (Start value of sum of digits)
+
 getch:	GETCHAR
 	cmp eax, -1
 	je break
@@ -16,10 +17,10 @@ getch:	GETCHAR
 	cmp al, '9'
 	jnbe getch
 	sub al, '0'
-	add ecx, eax
+	add ecx, eax		; ECX += EAX (Add a number to the sum)
 	jmp short getch
 
-break:	jecxz quit
+break:	jecxz quit		; Avoid iterating for value 0
 print:	PUTCHAR '*'
 	loop print
 
